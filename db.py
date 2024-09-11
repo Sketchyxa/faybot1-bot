1fe884bc2421a768d155fe3e0eedbb9a5746b70d
Sketchyxa <152073541+Sketchyxa@users.noreply.github.com>
2024-09-11 13:13:19 +0300

    Update db.py

--- db.py
+++ db.py
@@ -1,1 +1,37 @@
+import sqlite3
+from config import DATABASE
+
+def create_db():
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute('''CREATE TABLE IF NOT EXISTS users (
+                      user_id INTEGER PRIMARY KEY,
+                      balance INTEGER DEFAULT 5000)''')
+    conn.commit()
+    conn.close()
+
+def get_balance(user_id):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("SELECT balance FROM users WHERE user_id = ?", (user_id,))
+    result = cursor.fetchone()
+    conn.close()
+    if result:
+        return result[0]
+    else:
+        return None
+
+def update_balance(user_id, amount):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("UPDATE users SET balance = balance + ? WHERE user_id = ?", (amount, user_id))
+    conn.commit()
+    conn.close()
+
+def create_user(user_id):
+    conn = sqlite3.connect(DATABASE)
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO users (user_id) VALUES (?)", (user_id,))
+    conn.commit()
+    conn.close()
 
