dffa3b83945bae9278a91aba03aad464da15b01d
Sketchyxa <152073541+Sketchyxa@users.noreply.github.com>
2024-09-11 13:42:58 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 import telebot
 from config import BOT_TOKEN
 from handlers import register_handlers
+from db import initialize_db
+
+# Инициализация базы данных
+initialize_db()
 
 # Инициализация бота
 bot = telebot.TeleBot(BOT_TOKEN)
